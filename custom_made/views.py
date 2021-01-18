75e2538453459ac2f6d300d185fbf15719631e68
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-01-18 17:56:53 +0000

    Add custom_made forms and views

--- custom_made/views.py
+++ custom_made/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render, request
-from .models import CustomProduct, Fabric
+from django.shortcuts import render
 
-# Create your views here.
+from .forms import CustomProductForm
 
+
+def custom_made(request):
+    custom_product_form = CustomProductForm()
+    template = 'custom_made/custom_made.html'
+    context = {
+        'custom_product_form': custom_product_form
+    }
+
+    return render(request, template, context)
