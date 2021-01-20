2d9c139f705db34db79dc431687699106429d5b1
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-01-20 21:01:56 +0000

    Get to render fabrics images

--- custom_made/views.py
+++ custom_made/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
 from custom_made.forms import CustomProductForm
+from fabrics.models import Fabric
+from products.models import Product
 
 
 def custom_made_view(request):
+    products = Product.price_custom
+    fabrics = Fabric.objects.all()
     custom_product_form = CustomProductForm()
     template = 'custom_made/custom_made.html'
     context = {
+        'products': products,
+        'fabrics': fabrics,
         'custom_product_form': custom_product_form
     }
 
