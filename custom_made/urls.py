e6370a087c9e7189a5393393df8179d59f63f6e9
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-01-23 12:57:30 +0000

    Add select dropdown list for fabrics

--- custom_made/urls.py
+++ custom_made/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.custom_made_view, name='custom_made_view'),
+    path('<int:product_id>/', views.custom_made_view, name='custom_made_view'),
 ]
--- custom_made/views.py
+++ custom_made/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from custom_made.forms import CustomProductForm
 from fabrics.models import Fabric
 from products.models import Product
 
 
-def custom_made_view(request):
-    products = Product.objects.only('price_custom', 'name')
+def custom_made_view(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
     fabrics = Fabric.objects.all()
     custom_product_form = CustomProductForm()
     template = 'custom_made/custom_made.html'
     context = {
-        'products': products,
+        'product': product,
         'fabrics': fabrics,
         'custom_product_form': custom_product_form
     }
