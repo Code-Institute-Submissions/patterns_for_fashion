8d4b623dfd58021a88d470f51c5e07fce13f838a
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-02-08 20:29:56 +0000

    Add bag_contents_custom to bag contexts

--- bag/contexts.py
+++ bag/contexts.py
@@ -57,7 +57,7 @@ def bag_contents(request):
 
 def bag_contents_custom(request):
 
-    bag_items = []
+    bag_items_custom = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
@@ -67,7 +67,7 @@ def bag_contents_custom(request):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price_custom
             product_count += item_data
-            bag_items.append({
+            bag_items_custom.append({
                 'item_id': item_id,
                 'quantity': item_data,
                 'product': product,
@@ -77,7 +77,7 @@ def bag_contents_custom(request):
             for fabric, quantity in item_data['items_by_fabric'].items():
                 total += quantity * product.price_custom
                 product_count += quantity
-                bag_items.append({
+                bag_items_custom.append({
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
@@ -94,7 +94,7 @@ def bag_contents_custom(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
+        'bag_items_custom': bag_items_custom,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
