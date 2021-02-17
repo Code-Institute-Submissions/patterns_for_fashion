748c0ff761b750dec85f17292491f33e08ad7636
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-02-17 11:59:26 +0000

    Fix js for footer get full year copyright

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,9 @@
 $('.toast').toast('show');
+
+function copyrightYear() {
+   var d = new Date();
+   var y = d.getFullYear();
+   document.getElementById("copyright").innerHTML = y;
+}
+
+copyrightYear();
